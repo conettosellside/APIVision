f440b74954cdaa8207873b64f2a746aaef6d100e
conettosellside <cristian.onetto@sellside.cl>
2024-02-19 14:40:00 -0300

    Last changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&4y2c#(v4(8$d10#7l&*=a+vc-3xt@loo9#6*@mw^au$d0hnn3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
